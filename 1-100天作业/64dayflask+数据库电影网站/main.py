6646ae0c700d9a45b073a79bb689981e2bd7546f
Yang ruixiang <1264643521@qq.com>
2024-08-20 15:22:42 +0800

    8/20第64day

--- 1-100天作业/64dayflask+数据库电影网站/main.py
+++ 1-100天作业/64dayflask+数据库电影网站/main.py
@@ -5,7 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField,SubmitField
 from wtforms.validators import DataRequired
 import requests
-
+from get_title import get_title
 '''
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -21,15 +21,18 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['SQLALCHEMY_DATABASE_URI'] = r"sqlite:///C:\Users\杨年轻\Desktop\py\1-100天作业\64dayflask+数据库电影网站\moive-collection.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = r"sqlite:///C:\Users\Administrator\Desktop\py\1-100天作业\64dayflask+数据库电影网站\moive-collection.db"
 Bootstrap5(app)
 db = SQLAlchemy(app=app)
 
 class EditForm(FlaskForm):
-    rating = FloatField("rating",validators=[DataRequired()])
-    review = StringField("review",validators=[DataRequired()])
+    rating = FloatField("您的评价应在10分以内，如7.5分！",validators=[DataRequired()])
+    review = StringField("您的评论！",validators=[DataRequired()])
     submit = SubmitField('Done')
-
+    
+class AddForm(FlaskForm):
+    title = StringField("请输入电影名称！",validators=[DataRequired()])
+    submit = SubmitField('Done')
 
 class Movie(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -54,9 +57,38 @@ def edit():
     id = request.args.get("id")
     movie = db.get_or_404(Movie,id)
     form = EditForm()
-    
-    
+    if request.method == "POST":
+        rating = request.form.get('rating')
+        review = request.form.get('review')
+        movie = Movie.query.filter_by(id=id).first()
+        movie.rating = rating
+        movie.review = review
+        db.session.commit()
+        return redirect(url_for("home"))
     return render_template("edit.html",movie=movie,form=form)
+
+@app.route("/del",methods=["GET",'POST'])
+def dell():
+    id = request.args.get("id")
+    del_move = Movie.query.filter_by(id=id).first()
+    db.session.delete(del_move)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route("/add",methods=["POST","GET"])
+def add():
+    form = AddForm()
+    if request.method == "POST":
+        title = request.form.get("title")
+        datas = get_title(title)
+        print(datas)
+        return render_template("select.html",datas=datas)
+    return render_template("add.html",form=form)
+
+@app.route("/addscv",methods=["POST","GET"])
+def addscv():
+    data = request.args.get("data")
+    return render_template("indexs.html",data = data)
 
 
 if __name__ == '__main__':
