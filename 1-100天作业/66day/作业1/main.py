3f8f664bbbc43376791155ad30f754a999f024ed
Yang ruixiang <1264643521@qq.com>
2024-08-22 13:19:50 +0800

    完成65day！

--- 1-100天作业/66day/作业1/main.py
+++ 1-100天作业/66day/作业1/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 # from random import choice
 import random
@@ -51,8 +51,26 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return redirect('https://documenter.getpostman.com/view/37835189/2sAXjDduwB')
 
+@app.route("/add",methods=["GET",'POST'])
+def add():
+    new_cafe = Cafe(
+    name = str(request.form.get('name')),
+    map_url = str(request.form.get('map_url')),
+    img_url = str(request.form.get('img_url')),
+    location = str(request.form.get('location')),
+    seats = str(request.form.get('seats')),
+    has_toilet = bool(request.form.get('has_toilet')),
+    has_wifi = bool(request.form.get('has_wifi')),
+    has_sockets = bool(request.form.get('has_sockets')),
+    can_take_calls = bool(request.form.get('can_take_calls')),
+    coffee_price = str(request.form.get('coffee_price'))
+    )
+    print(new_cafe)
+    db.session.add(new_cafe)
+    db.session.commit()
+    return render_template("index.html")
 
 @app.route("/random",methods=["GET"])
 def randoms():
@@ -72,9 +90,31 @@ def all_cafe():
 
 @app.route("/search",methods=["GET"])
 def search():
-    result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
-    all_cafes = result.scalar()
+    loc=request.args.get('loc')
+    all_cafes = db.session.execute(db.select(Cafe).where(Cafe.location == loc)).scalars().all()
+    cafes_list = [ cafes.to_dict() for cafes in all_cafes ]
+    if cafes_list:
+        return jsonify({"cafes":cafes_list})
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."}),404
+    
+@app.route("/update-price/<int:cafe_id>",methods=["PATCH"])
+def pach(cafe_id):
+    data = db.session.execute(db.select(Cafe).where(Cafe.id == cafe_id)).scalar()
+    # data.price = request.args.get('nwe_price')
+    # data = db.get_or_404(Cafe,cafe_id)
+    if data:
+        data.coffee_price = request.args.get('nwe_price')
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."})
+    else: 
+        return jsonify({"error":{"Not Found":"Sorry a cafe with that id was not found in the database."} })
+    
+    # print(request.args.get('nwe_price'))
 
+    
+
+# http://127.0.0.1:5000/search?loc=Peckham
 # HTTP GET - Read Record
 
 # HTTP POST - Create Record
