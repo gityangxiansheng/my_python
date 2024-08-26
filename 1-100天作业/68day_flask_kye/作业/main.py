52ba194813ef2acae69abb42aeb43baf761ab8bd
young <1264643521@qq.com>
2024-08-26 17:23:27 +0800

    68day_flask_kye

--- 1-100天作业/68day_flask_kye/作业/main.py
+++ 1-100天作业/68day_flask_kye/作业/main.py
@@ -10,7 +10,9 @@ app.config['SECRET_KEY'] = 'secret-key-goes-here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/young/py/1-100天作业/68day_flask_kye/作业/instance/users.db'
 db = SQLAlchemy()
 db.init_app(app)
-
+# 创建并初始化 LoginManager 实例  
+login_manager = LoginManager()  
+login_manager.init_app(app)  
 
 # CREATE TABLE IN DB
 class User(db.Model):
@@ -24,11 +26,17 @@ with app.app_context():
     db.create_all()
 
 
+#创建回调
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+#主页
 @app.route('/')
 def home():
     return render_template("index.html")
 
-
+#注册页面
 @app.route('/register',methods=['GET','POST'])
 def register():
     print(request.method)
@@ -37,13 +45,14 @@ def register():
         new_user = User(
         name = name,
         email = request.form.get('email'),
-        password = request.form.get('password'))
+        password = generate_password_hash(request.form.get('password'),method="pbkdf2:sha256",salt_length=8))
+        
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('secrets',name=name ))
     return render_template("register.html")
 
-
+#登陆
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'POST':
@@ -52,8 +61,11 @@ def login():
         if  data :
             in_password = request.form.get('password')
             password = data.password
-            if in_password == password:
+            is_correct = check_password_hash(password, in_password)  
+            if is_correct:
                 print('登陆成功')
+                name = data.name
+                return redirect(url_for('secrets',name=name))
             else:
                 print(f'{in_password}密码错误')
         else:
@@ -63,7 +75,7 @@ def login():
     
     return render_template("login.html")
 
-
+#欢迎
 @app.route('/secrets')
 def secrets():
     name = request.args.get('name')
@@ -74,7 +86,7 @@ def secrets():
 def logout():
     pass
 
-
+#下载页
 @app.route('/download')
 def download():
     return send_from_directory('static', path="files/cheat_sheet.pdf")
